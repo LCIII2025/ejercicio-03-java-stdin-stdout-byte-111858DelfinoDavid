a329be8781b16cfdfe3f6c3b60931ba444a4290d
davedelf <davideliasdelfino@gmail.com>
2025-04-09 15:38:03 -0300

    done

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -1,5 +1,7 @@
 package ar.edu.utn.frc.tup.lciii;
 
+import java.util.InputMismatchException;
+import java.util.Optional;
 import java.util.Scanner;
 
 /**
@@ -11,13 +13,24 @@ public class App
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
-        byte a = scan.nextByte();
-        // Complete this line
-        // Complete this line
+        Optional<Byte> a=isByte(scan.nextLine());
+        Optional<Byte> b=isByte(scan.nextLine());
+        Optional<Byte> c=isByte(scan.nextLine());
 
-        System.out.println(a);
-        // Complete this line
-        // Complete this line
+        System.out.println(a.get().byteValue());
+        System.out.println(b.get().byteValue());
+        System.out.println(c.get().byteValue());
 
     }
+    public static Optional<Byte> isByte(String  in){
+        try{
+            byte value= Byte.parseByte(in);
+            return Optional.of(value);
+        }catch(NumberFormatException e){
+            throw new InputMismatchException();
+        }
+    }
+
+
+
 }
